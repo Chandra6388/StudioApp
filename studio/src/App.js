95a0867fba6728f74b69b2c273535ec12d58c75c
Chandra Prakash <cppatel6388@gmail.com>
2024-02-29 22:27:46 +0530

    done

--- studio/src/App.js
+++ studio/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from 'react-router-dom';
+import Navbar from './features/navbar/Navbar';
+// import Navbar from './features/navbar/Navbar';
+
+ 
+
+
+
+const router = createBrowserRouter([
+  // {
+  //   path: '/',
+  //   element: <Home />
+  // },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />
+  },
+   
+  
+
+
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+      <RouterProvider router={router} />
+      {/* <Navbar/> */}
+    </>
   );
 }
 
---
+++ studio/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../features/navbar/Navbar';
+ 
+
+ 
+const Home = () => {
+    return (
+        <div>
+            {/* <Navbar/>  */}
+        </div>
+    );
+}
+
+export default Home;
